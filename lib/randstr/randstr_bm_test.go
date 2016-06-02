5491b9bf0412373fc78ca70452db3ce552d7b094
Leigh McCulloch <leigh@mcchouse.com>
2016-06-02 06:11:15 -0700

    Add additional benchmarks with larges sources.
    What
    ===
    Add additional benchmarks with larges sources.
    
    Why
    ===
    Tests

--- lib/randstr/randstr_bm_test.go
+++ lib/randstr/randstr_bm_test.go
@@ -2,8 +2,17 @@ package randstr
 
 import "testing"
 
-func BenchmarkNew(b *testing.B) {
+func BenchmarkNewSmallSource(b *testing.B) {
+	runes := ASCIIRunes
 	for i := 0; i < b.N; i++ {
-		New(50, ASCIIRunes)
+		New(50, runes)
+	}
+}
+
+func BenchmarkNewLargeSource(b *testing.B) {
+	runesLit := [9999999]rune{}
+	runes := runesLit[:]
+	for i := 0; i < b.N; i++ {
+		New(50, runes)
 	}
 }
