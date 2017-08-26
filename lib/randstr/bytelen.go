828a65e339fd481e50c1626dec0f71b13367adeb
Leigh McCulloch <leigh@mcchouse.com>
2017-08-26 06:26:51 +0000

    Change byteLen to use new go1.9 math/bits
    What
    ===
    Change byteLen to use new go1.9 math/bits to calculate the minimum
    number of bytes that would be required to store an integer.
    
    Why
    ===
    It's faster.

--- lib/randstr/bytelen.go
+++ lib/randstr/bytelen.go
@@ -1,15 +1,8 @@
 package randstr
 
+import "math/bits"
+
 // byteLen returns the minimum number of bytes that would be required to store the integer.
 func byteLen(n int) int {
-	b := 0
-	s := 1
-	for s != 0 {
-		b++
-		s = s << 8
-		if n < s {
-			return b
-		}
-	}
-	return b
+	return (bits.Len(uint(n))-1)/8 + 1
 }
